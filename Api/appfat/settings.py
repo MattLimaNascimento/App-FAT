96ea4615d1b324588a05e9de209ed1d5ec13b2cb
Pedro <pedro.souza@discentes.fat.uerj.br>
2023-10-09 10:45:46 -0300

    commits 06/10

--- Api/appfat/settings.py
+++ Api/appfat/settings.py
@@ -144,8 +144,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-root')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # App-FAT
 Criação de um app com objetivo de conectar todo o campus UERJ!
-# API ( GUIA PARA LEIGOS):
+# API :
