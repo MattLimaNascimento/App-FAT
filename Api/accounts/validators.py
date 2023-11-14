da7d0556ae3bc4e9a7787cb876f927266e6ec4e4
Pedro Victor santos de Souza <pedro.souza@discentes.fat.uerj.br>
2023-11-14 12:16:48 -0300

    changes from yesterday

---
+++ Api/accounts/validators.py
@@ -0,0 +1,10 @@
+from validate_docbr import CNH
+from rest_framework import serializers
+
+def cnh_valido(value):
+        if value is not None:
+            cnh_validator = CNH()
+            if not cnh_validator.validate(value):
+                raise serializers.ValidationError('CNH inválida. Certifique-se de que o formato é válido.')
+        
+            
