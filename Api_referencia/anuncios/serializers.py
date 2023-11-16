bc2ba2565e997bd2451279d0887daa420b3e5081
pedrosouzacan <pedro.souza@discentes.fat.uerj.br>
2023-11-16 17:15:08 -0300

    Carrega foto de motorista em anuncios.serializers

--- Api_referencia/anuncios/serializers.py
+++ Api_referencia/anuncios/serializers.py
@@ -14,16 +14,16 @@ class CarregaDadosPassageirosSerializer(serializers.ModelSerializer):
 
 class RidesSerializer(serializers.ModelSerializer):
     passageiros = CarregaDadosPassageirosSerializer(many=True, required=False, read_only=True)
-    passageiros_id = serializers.PrimaryKeyRelatedField(
-        many=True,
-        read_only=False,
-        queryset=User.objects.all(),
-        source='passageiros'
-    )
+   # passageiros_id = serializers.PrimaryKeyRelatedField(
+    #     many=True,
+    #     read_only=False,
+    #     queryset=User.objects.all(),
+    #     source='passageiros'
+    # )
 
     class Meta:
         model = Ride
-        fields = ['id','motorista', 'passageiros', 'passageiros_id', 'data_publicaçao',
+        fields = ['id','motorista', 'passageiros', 'data_publicaçao',
                   'data_saida', 'origem', 'destino', 'preço', 'veiculo', 'modalidade']
 
     def update(self, instance, validated_data):
@@ -33,11 +33,25 @@ class RidesSerializer(serializers.ModelSerializer):
         for passageiro in passageiros:
             instance.passageiros.add(passageiro)
         return instance
+    
+class CarregaFotoMotoristaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'diretorio']
 
 class UserRidesSerializer(serializers.ModelSerializer):
-    class Meta :
+    motorista = CarregaFotoMotoristaSerializer(read_only=True)
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        motorista_data = representation['motorista']
+        representation['motorista'] = [motorista_data['id'], motorista_data['diretorio']]
+        return representation
+    
+    class Meta:
         model = Ride
-        fields = "__all__"
+        fields = ['motorista', 'passageiros', 'data_publicaçao',
+                  'data_saida', 'origem', 'destino', 'preço', 'veiculo', 'modalidade']
 
 class UserSerialier(serializers.ModelSerializer):
     class Meta:
