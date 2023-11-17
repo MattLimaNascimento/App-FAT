ccb2bbe1ef6a19e03e852c6951d9fb8b713983d3
Matheus <matheus.steven.9@gmail.com>
2023-11-17 20:03:58 -0300

    Commit 17/11

--- Api/account/serializers.py
+++
@@ -1,9 +0,0 @@
-from djoser.serializers import UserCreateSerializer
-from accounts.models import User
-
-
-class UserCreateSerializer(UserCreateSerializer):
-    class Meta(UserCreateSerializer.Meta):
-        model=User
-        fields=('id', 'email', 'name', 'diretorio', 'placa_carro', 'cnh', 'password')
-        
